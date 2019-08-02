305bdd10fe5217b7b90e6773cd216b914421ea31
MrShlyapnik <ajvarnabiullin@gmail.com>
2019-08-02 14:14:28 +0500

    Make Product model

--- shop/products/models.py
+++ shop/products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name=models.CharField(max_length=128)
+    description=models.TextField()
+    price=models.IntegerField()
+    supplier=models.CharField(max_length=64)
+    buy_count=models.IntegerField()
+
+    def __str__(self):
+        return self.name
--- shop/products/views.py
+++ shop/products/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from .models import Product
 # Create your views here.
 
 class IndexList(ListView):
     def get(self, request, *args, **kwargs):
+        model=Product
+        context_object_name = 'products'
+        objects=model.objects.all()
+        return render(request, 'products/index.html',{
+        'products': objects
+        })
+    def post(self, request, *args, **kwargs):
         return render(request, 'products/index.html')
